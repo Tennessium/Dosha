7e0de7ac6f04836c6aff8cc9e6a4a99556769c78
Tennessium <mighty.igor@yandex.ru>
2018-12-22 23:15:17 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -22,3 +22,20 @@ class Point:
 
     def __str__(self):
         return self.name + str(self.get_coords())
+
+
+class ColoredPoint(Point):
+    color = (0, 0, 0)
+
+    def __init__(self, name, x, y, color=(0, 0, 0)):
+        self.color = color
+        super().__init__(name, x, y)
+
+    def __invert__(self):
+        return ColoredPoint(self.name, self.y, self.x, (255 - self.color[0], 255 - self.color[1], 255 - self.color[2]))
+
+    def get_color(self):
+        return self.color
+
+    #def __str__(self):
+    #    return super().__str__() + " " + str(self.color)
