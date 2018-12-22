e7e418dbab15655cb8939f46917dce906524aa94
Tennessium <mighty.igor@yandex.ru>
2018-12-22 23:30:03 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,39 +1,36 @@
-class Point:
-    x = 0
-    y = 0
-    name = ''
+class BellTower:
+    kolokola = []
 
-    def __init__(self, name, x, y):
-        self.name = name
-        self.x = x
-        self.y = y
+    def __init__(self, *args):
+        for i in args:
+            self.kolokola.append(i)
 
-    def get_x(self):
-        return self.x
+    def append(self, x):
+        self.kolokola.append(x)
 
-    def get_y(self):
-        return self.y
-
-    def get_coords(self):
-        return (self.get_x(), self.get_y())
-
-    def __invert__(self):
-        return Point(self.name, self.y, self.x)
-
-    def __str__(self):
-        return self.name + str(self.get_coords())
+    def sound(self):
+        for i in self.kolokola:
+            i.sound()
+        print("...")
 
 
-class ColoredPoint(Point):
-    color = (0, 0, 0)
+class LittleBell:
+    def __init__(self):
+        pass
 
-    def __init__(self, name, x, y, color=(0, 0, 0)):
-        self.color = color
-        super().__init__(name, x, y)
+    def sound(self):
+        print("ding")
 
-    def __invert__(self):
-        ncolor = (255 - self.color[0], 255 - self.color[1], 255 - self.color[2])
-        return ColoredPoint(self.name, self.y, self.x, ncolor)
 
-    def get_color(self):
-        return self.color
+class BigBell:
+    ding = True
+
+    def __init__(self):
+        pass
+
+    def sound(self):
+        if self.ding:
+            print("ding")
+        else:
+            print("dong")
+        self.ding = not self.ding
