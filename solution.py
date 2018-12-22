5038b4bd534a4cdc1ace92be3acc79531fb08c14
Tennessium <mighty.igor@yandex.ru>
2018-12-23 00:09:24 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -11,23 +11,61 @@ class BellTower:
     def sound(self):
         for i in self.kolokola:
             i.sound()
-        if len(self.kolokola) != 0:
-            print("...")
+        print("...")
 
 
-class LittleBell:
-    def __init__(self):
-        pass
+class Bell:
+    def __init__(self, *args):
+        self.data = []
+        self.kdata = {}
+
+        for i in args:
+            if type(i) == type(str()):
+                self.data.append(i)
+            elif type(i) == type({}):
+                for j in i:
+                    self.kdata[j] = i[j]
+            elif type(i) == type(()):
+                for j in i:
+                    if type(j) == type(str()):
+                        self.data.append(j)
+
+    def print_info(self):
+        s = ""
+        self.data.sort()
+        keys = self.kdata.keys()
+        dd = []
+        for i in keys:
+            dd.append(i)
+
+        dd.sort()
+
+        for i in dd:
+            s += i + ': ' + self.kdata[i] + ', '
+        try:
+            s = s[:-2]
+        except Exception:
+            pass
+        if len(s) != 0:
+            s += '; '
+        for i in self.data:
+            s += i + ', '
+        print(s)
+
+
+class LittleBell(Bell):
+    def __init__(self, *args, **kwargs):
+        super().__init__(args, kwargs)
 
     def sound(self):
         print("ding")
 
 
-class BigBell:
+class BigBell(Bell):
     ding = True
 
-    def __init__(self):
-        pass
+    def __init__(self, *args, **kwargs):
+        super().__init__(args, kwargs)
 
     def sound(self):
         if self.ding:
