aead9c3bfeea58fb2ed5bf31d537485d88780bfd
Tennessium <mighty.igor@yandex.ru>
2018-12-22 22:46:04 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,6 +1,9 @@
+PITCHES = ["до", "ре", "ми", "фа", "соль", "ля", "си"]
+
+
 class Note:
     sound = ""
-    llong = False
+    is_long = False
     dick = {
         "до": "до-о",
         "ре": "ре-э",
@@ -11,15 +14,36 @@ class Note:
         "си": "си-и"
     }
 
-    def __init__(self, *args):
-        self.sound = args[0]
-        try:
-            self.llong = args[1]
-        except Exception:
-            pass
+    def __init__(self, sound="до", is_long=False):
+        self.sound = sound
+        self.is_long = is_long
 
     def __str__(self):
-        if not self.llong:
+        if not self.is_long:
             return self.sound
         else:
             return self.dick[self.sound]
+
+
+class LoudNote(Note):
+    def __str__(self):
+        return super().__str__().upper()
+
+
+class DefaultNote(Note):
+    def __init__(self, sound="до", is_long=False):
+        super().__init__(sound, is_long)
+
+    def __str__(self):
+        return super().__str__()
+
+
+class NoteWithOctave(Note):
+    octava = ""
+
+    def __init__(self, sound, octava="", is_long=False):
+        super().__init__(sound, is_long)
+        self.octava = octava
+
+    def __str__(self):
+        return super().__str__() + " (" + self.octava + ')'
