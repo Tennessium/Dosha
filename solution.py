56bf4c4703dd757c0c339f5dd2b3d77951a94b15
Tennessium <mighty.igor@yandex.ru>
2018-12-22 22:20:13 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,6 +1,6 @@
 class Note:
     sound = ""
-    long = False
+    llong = False
     dick = {
         "до": "до-о",
         "ре": "ре-э",
@@ -14,12 +14,12 @@ class Note:
     def __init__(self, *args):
         self.sound = args[0]
         try:
-            self.long = args[1]
+            self.llong = args[1]
         except Exception:
             pass
 
     def __str__(self):
-        if not self.long:
+        if not self.llong:
             return self.sound
         else:
             return self.dick[self.sound]
