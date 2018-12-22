063d588e86df8809c3d4a7fcc6065d3edb3ae620
Tennessium <mighty.igor@yandex.ru>
2018-12-22 22:16:16 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,5 +1,16 @@
 class Note:
+    sound = ""
+    long = False
+
     def __init__(self, *args):
-        print(args)
+        self.sound = args[0]
+        try:
+            self.long = args[1]
+        except Exception:
+            pass
 
-
+    def __str__(self):
+        if not self.long:
+            return self.sound
+        else:
+            return self.sound + "-" + self.sound[len(self.sound) - 1]
